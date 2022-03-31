2e8eca0ff7ae885e964b6d355fbae17a9393398c
Martin Pek <Martin.Pek@web.de>
2022-03-31 16:53:42 +0200

    stb_led: added colour functions

--- stb_led/src/stb_led.cpp
+++ stb_led/src/stb_led.cpp
@@ -3,9 +3,9 @@
 /**
  * @brief 
  * 
- * @param neopixel 
- * @param ledCnts 
- * @param pins 
+ * @param neopixel array of type Adafruit_NeoPixel
+ * @param ledCnts int array delcaring how many leds are on said strip
+ * @param pins  int array with the pins to be used
  * @param clrOrder defaults to NEO_RGB
  * @param clkSpeed defaults to NEO_KHZ800
  * @return true 
@@ -22,4 +22,30 @@ bool STB_LED::ledInit(Adafruit_NeoPixel neopixels[], int ledCnts[], int pins[], 
         }
     }
     return true;
+}
+
+/**
+ * @brief 
+ * 
+ * @param neopixel single led instance
+ * @param clr 
+ */
+void STB_LED::setStripToClr(Adafruit_NeoPixel &neopixel, long int clr) {
+    for (uint16_t ledNr=0; ledNr<neopixel.numPixels(); ledNr++) {
+        neopixel.setPixelColor(ledNr, clr);
+        neopixel.show();
+    }
+    delay(10);
+}
+
+/**
+ * @brief 
+ * 
+ * @param neopixels array of led instances
+ * @param clr 
+ */
+void STB_LED::setAllStripsToClr(Adafruit_NeoPixel neopixels[], long int clr) {
+    for (size_t i=0; i<sizeof(neopixels); i++) {
+        STB_LED::setStripToClr(neopixels[i], clr);
+    }
 }
--- stb_led/src/stb_led.h
+++ stb_led/src/stb_led.h
@@ -4,5 +4,6 @@
 class STB_LED {
     public:
     static bool ledInit(Adafruit_NeoPixel neopixels[], int ledCnts[], int pins[], uint32_t clrOrder=NEO_RGB, int clkSpeed=NEO_KHZ800);
-
+    static void setStripToClr(Adafruit_NeoPixel &neopixel, long int clr);
+    static void setAllStripsToClr(Adafruit_NeoPixel neopixels[], long int clr);
 };
