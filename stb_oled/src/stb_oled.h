3a954ecc51f62da7aa5e29c627760604cfee31df
MartinPek <martin_pek@web.de>
2022-04-13 17:42:02 +0200

    fixed oled init

--- stb_oled/src/stb_oled.h
+++ stb_oled/src/stb_oled.h
@@ -2,7 +2,7 @@
 
 class STB_OLED {
     public:
-    static bool oledInit(SSD1306AsciiWire oled, DevType oledType, int oledAddr=0x3C);
+    static bool oledInit(SSD1306AsciiWire *oled, DevType oledType, int oledAddr=0x3C);
     static void systemPrint(SSD1306AsciiWire oled, String text);
     static void systemPrintln(SSD1306AsciiWire oled, String text);
 };
