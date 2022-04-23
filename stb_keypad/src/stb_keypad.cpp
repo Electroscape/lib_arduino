b2f08433f29ab5e60f13c9b68ba0fdcac726be03
MartinPek <martin_pek@web.de>
2022-04-23 19:04:47 +0200

    stb_keypad created

---
+++ stb_keypad/src/stb_keypad.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file stb_keypad.h
+ * @author Martin Pek (martin.pek@web.de)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "stb_keypad.h"
+ 
+
+STB_KEYPAD::STB_KEYPAD() {}
+
+bool STB_KEYPAD::STB_KEYPAD(int KEYPAD_ADD) {
+    // Keypad_I2C Keypad(makeKeymap(KeypadKeys), KeypadRowPins, KeypadColPins, KEYPAD_ROWS, KEYPAD_COLS, KEYPAD_ADD, PCF8574);
+}
---
+++ stb_keypad/src/stb_keypad.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Keypad_I2C.h>
+
+class STB_KEYPAD {
+    private:
+    const byte rows = 4;
+    const byte cols = 3;
+    const byte KEYPAD_CODE_LENGTH = 4;
+    const byte KEYPAD_CODE_LENGTH_MAX = 7;
+
+    char KeypadKeys[rows][cols] = {
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'},
+        {'*', '0', '#'}
+    };
+
+    byte KeypadRowPins[rows] = {1, 6, 5, 3};  // Zeilen  - Messleitungen
+    byte KeypadColPins[cols] = {2, 0, 4};     // Spalten - Steuerleitungen (abwechselnd HIGH)
+
+    public:
+
+    Keypad_I2C Keypad;
+    STB_KEYPAD();
+    bool init(int KEYPAD_ADD);
+}
