c25a78604ec90a975b6017ed5572b3217166c73f
Martin Pek <Martin.Pek@web.de>
2022-04-24 02:08:15 +0200

    stb_keypad: config values and constructor

--- stb_keypad/src/stb_keypad.cpp
+++ stb_keypad/src/stb_keypad.cpp
@@ -12,8 +12,25 @@
 #include "stb_keypad.h"
  
 
-STB_KEYPAD::STB_KEYPAD() {}
-
-bool STB_KEYPAD::STB_KEYPAD(int KEYPAD_ADD) {
-    // Keypad_I2C Keypad(makeKeymap(KeypadKeys), KeypadRowPins, KeypadColPins, KEYPAD_ROWS, KEYPAD_COLS, KEYPAD_ADD, PCF8574);
+STB_KEYPAD::STB_KEYPAD() {
+    Keypad_I2C Keypad(makeKeymap(KeypadKeys), KeypadRowPins, KeypadColPins, rows, cols, 0x39, PCF8574_MODE);
 }
+
+bool STB_KEYPAD::init(int keypadAddr) {
+    // 
+    return true;
+}
+
+
+
+/*
+
+void (*listener)(char)
+bool keypadInit() {
+    Keypad.addEventListener(keypadEvent);  // Event Listener erstellen
+    Keypad.begin(makeKeymap(KeypadKeys));
+    Keypad.setHoldTime(5000);
+    Keypad.setDebounceTime(20);
+    return true;
+}
+*/
