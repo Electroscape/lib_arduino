185fcd42cb436907061b7fb0c67d4bb4ee7d9e34
Martin Pek <Martin.Pek@web.de>
2022-04-24 02:08:15 +0200

    stb_keypad: config values and constructor

--- stb_keypad/src/stb_keypad.h
+++ stb_keypad/src/stb_keypad.h
@@ -4,12 +4,12 @@
 
 class STB_KEYPAD {
     private:
-    const byte rows = 4;
-    const byte cols = 3;
-    const byte KEYPAD_CODE_LENGTH = 4;
-    const byte KEYPAD_CODE_LENGTH_MAX = 7;
+    static constexpr byte rows = 4;
+    static constexpr byte cols = 3;
+    static constexpr byte KEYPAD_CODE_LENGTH = 4;
+    static constexpr byte KEYPAD_CODE_LENGTH_MAX = 7;
 
-    char KeypadKeys[rows][cols] = {
+    static constexpr char KeypadKeys[rows][cols] = {
         {'1', '2', '3'},
         {'4', '5', '6'},
         {'7', '8', '9'},
@@ -20,8 +20,7 @@ class STB_KEYPAD {
     byte KeypadColPins[cols] = {2, 0, 4};     // Spalten - Steuerleitungen (abwechselnd HIGH)
 
     public:
-
-    Keypad_I2C Keypad;
+    Keypad_I2C* Keypad = nullptr;
     STB_KEYPAD();
-    bool init(int KEYPAD_ADD);
-}
+    bool init(int keypadAddr);
+};
