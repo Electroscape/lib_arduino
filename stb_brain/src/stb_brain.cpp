ccb580b75940f3646dcd09f866204d8b5a7990b7
Martin Pek <Martin.Pek@web.de>
2022-07-20 22:53:14 +0200

    fixed value readout with usage of strncmp

--- stb_brain/src/stb_brain.cpp
+++ stb_brain/src/stb_brain.cpp
@@ -37,19 +37,28 @@ bool STB_BRAIN::receiveFlags(STB STB) {
             // use the char length to check this
             if (strncmp((char *) Keywords.flagKeyword, STB.rcvdPtr, strlen(Keywords.flagKeyword)) == 0) {
                 
+                STB.rcvdPtr += strlen(Keywords.flagKeyword);
+                STB.dbgln(STB.rcvdPtr);
+
                 char line[16] = "";
                 char* linePtr = strtok(line, "_"); 
                 linePtr = strtok(NULL, "_"); 
                 STB.dbgln("Flagkeyword receivd");
                 // add a safety check here
                 STB.dbgln(linePtr);
+            
+                char noString[2];
 
                 for (int keywordNo=0; keywordNo<cmdFlags::AmountOfFlags; keywordNo++) { 
-                    if (keywordNo == (signed char) linePtr[strlen(Keywords.flagKeyword)]) {
-                       STB.dbgln("correct keyword");
-                       flags[keywordNo] = (int) STB.rcvdPtr[strlen(Keywords.flagKeyword) + 2];
-                       sendAck = true;
-                       break;
+                    sprintf(noString, "%i", keywordNo);
+                    if (strncmp(STB.rcvdPtr, noString, 1) == 0) {
+                        STB.dbg("correct keyword for: ");
+                        STB.dbgln(String(keywordNo));
+                        STB.rcvdPtr += 2;
+                        sprintf(noString, "%i", 1);
+                        flags[keywordNo] = (strncmp(STB.rcvdPtr, noString, 1) == 0);
+                        sendAck = true;
+                        break;
                     }
                 }
             }
