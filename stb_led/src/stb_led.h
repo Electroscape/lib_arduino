ed37f92681a7cc85cb7e11884308963ebc4666f0
Martin Pek <martin.pek@web.de>
2024-03-21 17:06:29 +0100

    changed led_cnt from analyzer_fix merge back to 60
    merge also contained some minor fixes hence the merge in the first place

--- stb_led/src/stb_led.h
+++ stb_led/src/stb_led.h
@@ -7,7 +7,7 @@
 
 #define STRIPE_CNT 4
 // due to complile time requirements we cannot dynamically allocate the amount
-#define LED_MAX_CNT 3
+#define LED_MAX_CNT 60
 
 class STB_LED {
     private:
