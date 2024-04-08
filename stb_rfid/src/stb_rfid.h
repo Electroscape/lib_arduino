b75e1005f7fea4c8c6b0a25ec5d3129187f07c82
Martin Pek <martin.pek@web.de>
2024-04-08 12:55:55 +0200

    changed RFID timeout to 150

--- stb_rfid/src/stb_rfid.h
+++ stb_rfid/src/stb_rfid.h
@@ -15,7 +15,7 @@ class STB_RFID {
     public:
     static bool RFIDInit(Adafruit_PN532 &reader);
     static bool cardDetect(Adafruit_PN532 &reader, uint8_t *uid);
-    static bool cardRead(Adafruit_PN532 &reader, uint8_t data[16], int datablock=1, uint16_t timeout=250);
+    static bool cardRead(Adafruit_PN532 &reader, uint8_t data[16], int datablock=1, uint16_t timeout=150);
     void allRFID_Message(Adafruit_PN532 RFID_READERS[4], int rfid_amount, char* inputStr, int datablock=1); 
         // static bool RFID_Gate_locked();
         // static bool read_PN532(int reader_nr, uint8_t *data, uint8_t *uid, uint8_t uidLength);
